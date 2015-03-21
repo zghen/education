cdb9233cb8f98390b3ec73af72b99e9e84819abd
Paul Yehorov <pay@qapint.com>
2015-03-21 23:37:22 +0200

    create button navigation functionality

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
-var earth = "Земля";
-var visitor = "Петя";
+function next(){
+document.getElementById("cur").className = "page left first";
+document.getElementById("ri").className = "page current second";
+}
+
+function prev(){
+document.getElementById("cur").className = "page current first";
+document.getElementById("ri").className = "page right second";
+}
+
