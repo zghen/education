e14504106b58cd6306db719026b0d76650ea45ca
Paul <pay@qapint.com>
2015-03-25 18:25:58 +0200

    element adding refactoring, bugs fixing,  second page functionality partial adding

--- js/app.js
+++ js/app.js
@@ -1,95 +1,88 @@
 var indicator1 = document.getElementsByClassName('indicators indicator1 active')[0],
     indicator2 = document.getElementsByClassName('indicators indicator2')[0],
-    firstpage = document.getElementById('first-page'),
-    secondpage = document.getElementById('second-page'),
-    add1 = document.getElementById('add1'),
-    add2 = document.getElementById('add2'),
-    add3 = document.getElementById('add3'),
-    checkbox1 = document.querySelectorAll('.documents input[type=checkbox]'),
-    checkbox2 = document.querySelectorAll('.links input[type=checkbox]'),
-    checkbox3 = document.querySelectorAll('.science-lab input[type=checkbox]');
-    checkboxall = document.querySelectorAll('#first-page input[type=checkbox]');
+    firstPage = document.getElementById('first-page'),    
+    secondPage = document.getElementById('second-page'),
+    secondPageAttachment = document.querySelector('#second-page .attachment'),
+    checkboxCol = document.getElementsByClassName('col'),
+    sourceList = document.querySelectorAll('#first-page li');
+    counter = secondPage.getElementsByClassName('attachment-counter')[0],
 
 
-
-indicator2.addEventListener('click', function(){
-  firstpage.classList.remove('current');
-  secondpage.classList.remove('right');
-  firstpage.classList.add('left');
-  secondpage.classList.add('current');
-  indicator1.classList.remove('active');
-  indicator2.classList.add('active');
+  indicator2.addEventListener('click', function(){
+    firstPage.classList.remove('current');
+    secondPage.classList.remove('right');
+    firstPage.classList.add('left');
+    secondPage.classList.add('current');
+    indicator1.classList.remove('active');
+    indicator2.classList.add('active');
   });
 
-indicator1.addEventListener('click', function(){
-  firstpage.classList.remove('left');
-  secondpage.classList.remove('current');
-  firstpage.classList.add('current');
-  secondpage.classList.add('right');
-  indicator2.classList.remove('active');
-  indicator1.classList.add('active');
-});
+  indicator1.addEventListener('click', function(){
+    firstPage.classList.remove('left');
+    secondPage.classList.remove('current');
+    firstPage.classList.add('current');
+    secondPage.classList.add('right');
+    indicator2.classList.remove('active');
+    indicator1.classList.add('active');
+  });
 
-add1.addEventListener('click', function(){
-  if (add1.classList.contains('addall')){
-  add1.classList.remove('addall');
-  add1.classList.add('removeall');
-  add1.innerHTML="remove all";
-  [].forEach.call(checkbox1, function(item, index){
-    checkbox1[index].setAttribute("checked", "true");
-    });
-  }
-  else {
-  [].forEach.call(checkbox1, function(item, index){
-    checkbox1[index].removeAttribute('checked');
-    });
-  add1.classList.remove('removeall');
-  add1.classList.remove('addall1');
-  add1.classList.add('addall');
-  add1.classList.add('addall1');
-  add1.innerHTML="add all";  
-  } 
-});
+function fireEvent(element,event){
+    var evt = document.createEvent("HTMLEvents");
+    evt.initEvent(event, true, true ); // event type,bubbling,cancelable
+    return !element.dispatchEvent(evt);
+}
 
-add2.addEventListener('click', function(){
-  if (add2.classList.contains('addall')){
-  add2.classList.remove('addall');
-  add2.classList.add('removeall');
-  add2.innerHTML="remove all";
-  [].forEach.call(checkbox1, function(item, index){
-    checkbox2[index].setAttribute("checked", "true");
-    });
-  }
-  else {
-  [].forEach.call(checkbox1, function(item, index){
-    checkbox2[index].removeAttribute('checked');
-    });
-  add2.classList.remove('removeall');
-  add2.classList.remove('addall1');
-  add2.classList.add('addall');
-  add2.classList.add('addall1');
-  add2.innerHTML="add all";  
-  } 
-});
+  [].forEach.call(checkboxCol, function(col){
+      var addAll = col.querySelector('.addall'),
+          checkboxes = col.querySelectorAll('input[type=checkbox]');
 
-add3.addEventListener('click', function(){
-  if (add3.classList.contains('addall')){
-  add3.classList.remove('addall');
-  add3.classList.add('removeall');
-  add3.innerHTML="remove all";
-  [].forEach.call(checkbox1, function(item, index){
-    checkbox3[index].setAttribute("checked", "true");
-    });
-  }
-  else {
-  [].forEach.call(checkbox1, function(item, index){
-    checkbox3[index].removeAttribute('checked');
-    });
-  add3.classList.remove('removeall');
-  add3.classList.remove('addall1');
-  add3.classList.add('addall');
-  add3.classList.add('addall1');
-  add3.innerHTML="add all";  
-  } 
-});
+      addAll.addEventListener('click', function(){
+          if(addAll.classList.contains('removeall')){
+            addAll.classList.remove('removeall');
+            addAll.innerHTML = 'add all';
+          }else{
+            addAll.classList.add('removeall');
+            addAll.innerHTML = 'remove all';
+          }
 
+  [].forEach.call(checkboxes, function(checkbox){
+        if(addAll.classList.contains('removeall')){
+          checkbox.checked = true;
+          fireEvent(checkbox, 'change');
+        }else{
+          checkbox.checked = false;
+          fireEvent(checkbox, 'change');
+        }
+      });
+    });
+  });
+
+  [].forEach.call(sourceList, function(li, index){
+    var checkbox = li.querySelector('input[type=checkbox]'),
+        p1 = li.querySelectorAll('p')[0],
+        p2 = li.querySelectorAll('p')[1];
+        count = 0;
+  checkbox.addEventListener('change', function(event){
+    var li = document.createElement('li'),
+      createdLi = secondPage.querySelector('[index="' + index + '"]');
+        if(checkbox.checked && !createdLi){
+          li.setAttribute('index', index);
+          li.classList.add('attached');
+          li.innerHTML = "<p>" + p1.innerHTML + "</p><p>" + p2.innerHTML + "<button class=\"trashcan\"></button>" + "</p>";
+          li.querySelector('button').addEventListener('click', function(){
+            checkbox.checked = false;
+            fireEvent(checkbox, 'change');
+            count = count - 1;
+            counter.innerHTML = count  + " attached files";
+            });
+          secondPageAttachment.appendChild(li);                    
+          count = count + 1;
+          counter.innerHTML = count  + " attached files";
+        }else{
+          if(createdLi){
+            secondPageAttachment.removeChild(createdLi);
+            fireEvent(checkbox, 'change');
+          }
+        }
+    })
+});
