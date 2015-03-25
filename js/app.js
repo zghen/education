00cfc880195a120487da175ebc8303ae90689096
Paul <pay@qapint.com>
2015-03-25 13:10:54 +0200

    block2 added, navigation changed, Remove All functionality added

--- js/app.js
+++ js/app.js
@@ -1,32 +1,27 @@
-var prev = document.getElementsByClassName('goto-prev')[0],
-    next = document.getElementsByClassName('goto-next')[0],
-    indicator1 = document.getElementsByClassName('indicators indicator1 active')[0],
+var indicator1 = document.getElementsByClassName('indicators indicator1 active')[0],
     indicator2 = document.getElementsByClassName('indicators indicator2')[0],
     firstpage = document.getElementById('first-page'),
     secondpage = document.getElementById('second-page'),
-    addall1 = document.getElementsByClassName('addall addall1')[0],
-    addall2 = document.getElementsByClassName('addall addall2')[0],
-    addall3 = document.getElementsByClassName('addall addall3')[0],
+    add1 = document.getElementById('add1'),
+    add2 = document.getElementById('add2'),
+    add3 = document.getElementById('add3'),
     checkbox1 = document.querySelectorAll('.documents input[type=checkbox]'),
     checkbox2 = document.querySelectorAll('.links input[type=checkbox]'),
-    checkbox3 = document.querySelectorAll('.science-lab input[type=checkbox]'),
-    removeall1 = document.getElementsByClassName('addall1 removeall')[0],
-    removeall2 = document.getElementsByClassName('addall2 removeall')[0],
-    removeall3 = document.getElementsByClassName('addall3 removeall')[0];
+    checkbox3 = document.querySelectorAll('.science-lab input[type=checkbox]');
+    checkboxall = document.querySelectorAll('#first-page input[type=checkbox]');
 
 
 
-
-next.addEventListener('click', function(){
+indicator2.addEventListener('click', function(){
   firstpage.classList.remove('current');
   secondpage.classList.remove('right');
   firstpage.classList.add('left');
   secondpage.classList.add('current');
   indicator1.classList.remove('active');
   indicator2.classList.add('active');
-});
+  });
 
-prev.addEventListener('click', function(){
+indicator1.addEventListener('click', function(){
   firstpage.classList.remove('left');
   secondpage.classList.remove('current');
   firstpage.classList.add('current');
@@ -35,40 +30,66 @@ prev.addEventListener('click', function(){
   indicator1.classList.add('active');
 });
 
-addall1.addEventListener('click', function(){
-	addall1.innerHTML="remove all";
-	addall1.classList.remove('addall');
-  	addall1.classList.add('removeall');
+add1.addEventListener('click', function(){
+  if (add1.classList.contains('addall')){
+  add1.classList.remove('addall');
+  add1.classList.add('removeall');
+  add1.innerHTML="remove all";
   [].forEach.call(checkbox1, function(item, index){
-    checkbox1[index].setAttribute("checked", "checked");
+    checkbox1[index].setAttribute("checked", "true");
     });
-});
-
-addall2.addEventListener('click', function(){
-	addall2.innerHTML="remove all";
-  	addall2.classList.remove('addall');
-  	addall2.classList.add('removeall');
-  [].forEach.call(checkbox2, function(item, index){
-    checkbox2[index].setAttribute("checked", "checked");
-    });
-});
-
-addall3.addEventListener('click', function(){
-  addall3.classList.remove('addall');
-  addall3.classList.add('removeall');
-  addall3.innerHTML="remove all";
-  [].forEach.call(checkbox3, function(item, index){
-   	checkbox3[index].setAttribute("checked", "checked");
-    });
-});
-
-removeall1.addEventListener('click', function(){
-	removeall1.innerHTML="add all";
-	removeall1.classList.remove('removeall');
-  	removeall1.classList.add('addall');
+  }
+  else {
   [].forEach.call(checkbox1, function(item, index){
-    checkbox1[index].setAttribute("checked", "unchecked");
+    checkbox1[index].removeAttribute('checked');
     });
+  add1.classList.remove('removeall');
+  add1.classList.remove('addall1');
+  add1.classList.add('addall');
+  add1.classList.add('addall1');
+  add1.innerHTML="add all";  
+  } 
 });
 
+add2.addEventListener('click', function(){
+  if (add2.classList.contains('addall')){
+  add2.classList.remove('addall');
+  add2.classList.add('removeall');
+  add2.innerHTML="remove all";
+  [].forEach.call(checkbox1, function(item, index){
+    checkbox2[index].setAttribute("checked", "true");
+    });
+  }
+  else {
+  [].forEach.call(checkbox1, function(item, index){
+    checkbox2[index].removeAttribute('checked');
+    });
+  add2.classList.remove('removeall');
+  add2.classList.remove('addall1');
+  add2.classList.add('addall');
+  add2.classList.add('addall1');
+  add2.innerHTML="add all";  
+  } 
+});
+
+add3.addEventListener('click', function(){
+  if (add3.classList.contains('addall')){
+  add3.classList.remove('addall');
+  add3.classList.add('removeall');
+  add3.innerHTML="remove all";
+  [].forEach.call(checkbox1, function(item, index){
+    checkbox3[index].setAttribute("checked", "true");
+    });
+  }
+  else {
+  [].forEach.call(checkbox1, function(item, index){
+    checkbox3[index].removeAttribute('checked');
+    });
+  add3.classList.remove('removeall');
+  add3.classList.remove('addall1');
+  add3.classList.add('addall');
+  add3.classList.add('addall1');
+  add3.innerHTML="add all";  
+  } 
+});
 
